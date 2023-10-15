6bf6ed1509e084b69644942d6b192f31dca68a2b
Bozhidar Bozhilov <bozhidarbozhilov@gmail.com>
2023-10-15 17:55:35 +0300

    Fix fill first week logic

--- calendar.js
+++ calendar.js
@@ -4,32 +4,30 @@ const monthNames = ["January", "February", "March", "April", "May", "June","July
     const currentMonth = document.getElementById("month-name").innerText;
     const currentMonthIndex = monthNames.indexOf(currentMonth);
     const currentYear = document.getElementById("year-value").innerText;
-    fillMonth(currentDate.getMonth(), currentDate.getFullYear());
+    //fillMonth(currentDate.getMonth(), currentDate.getFullYear());
+    fillMonth(9,2023);
     const prevBtn = document.getElementById("prev-btn");
     const nextBtn = document.getElementById("next-btn");
-    prevBtn.addEventListener("click",()=>{
- 
-        const allWeeks = document.getElementsByClassName("week");
-        const firstWeekDays = allWeeks[0].getElementsByClassName("date");
-
-        for(let i = 0; i < firstWeekDays.length; i++){
-            firstWeekDays[i];
-        }
-        for(let i = 1; i < allWeeks.lengt;i++){
-            allWeeks[i];
-        }
-        fillMonth(8, currentYear);
-        if(currentMonth){
-            
-        }
-    });
+    // prevBtn.addEventListener("click",()=>{
+        
+    //     fillMonth(8,2023);
+    // });
 }
 )();
 
+function refreshContent(){
+    const generatedContent = document.getElementById("generated-tags");
+    generatedContent.innerHTML='';
+    const markedDates = document.getElementsByClassName("pn-month");
+    Array.from(markedDates).forEach(el=> el.classList.remove("pn-month"));
+    
+}
+
 function fillMonth(month, year){
+    //refreshContent();
     const monthEl = document.getElementById("month-name");
     const yearEl = document.getElementById("year-value");
-    const calendarContainer = document.getElementById("calendar-container");
+    const generatedContent = document.getElementById("generated-tags");
     monthEl.innerText = monthNames[month];
     yearEl.innerText = year;
     const firstDay = new Date(year, month, 1);
@@ -39,8 +37,10 @@ function fillMonth(month, year){
     const firstWeek = Array.from(document.getElementsByClassName("date"));
     const monthLength = daysInMonth(month, year);
     let currentDate = 1;
-    let firstWeekDay = new Date(year, month - 1, -(firstIndex-1)).getUTCDate();
-    
+    const previousMonthSlice = monthLength - firstIndex + 2;
+    let firstWeekDay = new Date(year, month - 1, previousMonthSlice).getUTCDate();
+    console.log(monthLength);
+    console.log(firstIndex);
     // fill the begining dates
     for(let i = 0; i < firstIndex; i++){
         firstWeek[i].classList.add("pn-month");
@@ -58,23 +58,26 @@ function fillMonth(month, year){
         if(i%7 == 0){
             weekEl = document.createElement("div");
             weekEl.classList.add("week");
-            calendarContainer.appendChild(weekEl);
+            generatedContent.appendChild(weekEl);
         }
         const dayEl = document.createElement("div");
         dayEl.classList.add("date");
         dayEl.innerText = currentDate + i;
         weekEl.appendChild(dayEl);
+        
     }
     // fill last week
-    const allWeeks = document.getElementsByClassName("week");
-    const lastWeek = allWeeks[allWeeks.length - 1];
-    for(let i = lastIndex + 1; i < 7; i++){
-        const dayEl = document.createElement("div");
-        dayEl.classList.add("date");
-        dayEl.classList.add("pn-month");
-        dayEl.innerText = i - lastIndex;
-        lastWeek.appendChild(dayEl);
-    }
+    // const allWeeks = document.getElementsByClassName("week");
+    // const lastWeek = allWeeks[allWeeks.length - 1];
+    // for(let i = lastIndex + 1; i < 7; i++){
+    //     console.log(lastIndex);
+    //     debugger;
+    //     const dayEl = document.createElement("div");
+    //     dayEl.classList.add("date");
+    //     dayEl.classList.add("pn-month");
+    //     dayEl.innerText = i - lastIndex;
+    //     lastWeek.appendChild(dayEl);
+    // }
 
 }
 function daysInMonth (month, year) {
